c5f93626ff959d3e45005512f626a5dd7aeb1e3a
knmfr <chenjiax@bu.edu>
2026-05-03 13:38:40 +0800

    202605030138

--- 算法/杂项技巧/常用/初级/旋转字符串/kmp.py
+++ 算法/杂项技巧/常用/初级/旋转字符串/kmp.py
@@ -13,4 +13,39 @@ from collections import defaultdict, deque, Counter
 fmin = lambda x, y: x if x < y else y
 fmax = lambda x, y: x if x > y else y
 
+# leetcode 796(https://leetcode.cn/problems/rotate-string/description/)
+# 旋转字符串本质是s的后缀和前缀拼接而成,因此其一定在s+s中
+# 由此转化成字符串匹配问题,可以用暴力或者kmp解决
+
+#解法1:暴力
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        return len(s)==len(goal) and goal in s+s
+
+#解法2:kmp
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        def kmp(s, p):
+            n, m = len(s), len(p)
+            ne = [0] * m
+            j = 0
+            for i in range(1, m):
+                while j and p[j] != p[i]:
+                    j = ne[j - 1]
+                if p[j] == p[i]:
+                    j += 1
+                ne[i] = j
+
+            j = 0
+            for i, v in enumerate(s):
+                while j and p[j] != v:
+                    j = ne[j - 1]
+                if p[j] == v:
+                    j += 1
+                if j == m:
+                    return i - m + 1
+            return -1
+
+        return len(s) == len(goal) and kmp(s + s, goal) != -1
+
 
